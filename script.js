c87ffd41704c7e16e0ac1c0a6150a54718808826
blumenlober <schneeschmidtchen@gmail.com>
2026-08-03 23:48:19 +0200

    java neu

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 const flower = document.getElementById("flower");
 
-fetch("blueten.json")
-    .then(response => response.json())
-    .then(images => {
+const STORAGE_KEY = "blueten-privat";
 
-        const randomIndex = Math.floor(Math.random() * images.length);
+async function start() {
 
-        flower.src = "blueten/" + images[randomIndex];
+    const response = await fetch("blueten.json");
+    const images = await response.json();
 
-    })
-    .catch(error => {
+    let state = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-        console.error("Fehler beim Laden der Blüten:", error);
+    // Beim ersten Start oder wenn alle Blüten gezeigt wurden
+    if (!state || state.remaining.length === 0) {
 
-    });
+        const shuffled = [...images];
+
+        shuffle(shuffled);
+
+        state = {
+            remaining: shuffled
+        };
+    }
+
+    const nextFlower = state.remaining.pop();
+
+    flower.src = "blueten/" + nextFlower;
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
+}
+
+function shuffle(array) {
+
+    for (let i = array.length - 1; i > 0; i--) {
+
+        const j = Math.floor(Math.random() * (i + 1));
+
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+start();
